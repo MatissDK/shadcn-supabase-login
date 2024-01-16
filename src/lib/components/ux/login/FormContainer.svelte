2a815281ddada258dc81a1ebf083dd1b24bbb58d
Matiss <matiss.aprups@fleetcomplete.com>
2024-01-16 15:48:08 +0200

    update signup background v2

--- src/lib/components/ux/login/FormContainer.svelte
+++ src/lib/components/ux/login/FormContainer.svelte
@@ -11,7 +11,7 @@
 			class="absolute inset-0 bg-cover"
 			style="
 				background-image:
-					url(/background-photo.avif);"
+					url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80);"
 		/>
 		<div class="relative z-20 flex items-center text-lg font-medium">
 			<a href="/">{config.title}</a>
